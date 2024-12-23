3a689fb7ab27a925d58af1a08d2e3c2c68d15c83
viktoriya-tsygler-idf <viktoriya.tsygler@idftechnology.by>
2024-12-23 20:55:29 +0300

    Initial commit

---
+++ src/main/java/by/itacademy/tsyhler/Main.java
@@ -0,0 +1,16 @@
+package by.itacademy.tsyhler;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> cities = new ArrayList<>();
+        cities.add("Warsaw");
+        cities.add("Minsk");
+        cities.add("Gomel");
+        cities.add("Praga");
+        cities.add("Dubai");
+        cities.add("Phuket");
+        System.out.println(cities);
+    }
+}
