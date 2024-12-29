5f4936be2fe8aefa7ef55dd1948556daecc176b1
viktoriya-tsygler-idf <viktoriya.tsygler@idftechnology.by>
2024-12-29 18:55:57 +0300

    add call method cityStartsWithLetter to Main.

--- src/main/java/by/itacademy/tsyhler/Main.java
+++ src/main/java/by/itacademy/tsyhler/Main.java
@@ -7,15 +7,17 @@ public class Main {
         ArrayList<String> cities = new ArrayList<>();
         cities.add("Warsaw");
         cities.add("Minsk");
-        cities.add("Gomel");
+        cities.add("Ufa");
         cities.add("Praga");
         cities.add("Dubai");
         cities.add("Phuket");
         System.out.println(cities);
-        cities.set(2, "Saratov");
+        cities.set(1, "Saratov");
         System.out.println(cities);
         String cityWithMaxLength = Util.cityWithMaxLength(cities);
         System.out.println(cityWithMaxLength);
         System.out.println(Util.cityWithMinLength(cities));
+        System.out.println(Util.cityStartsWithLetter(cities,'p'));
+        System.out.println(Util.cityStartsWithLetter(cities,'P'));
     }
 }
